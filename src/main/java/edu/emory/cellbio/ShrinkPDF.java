ef695e5e4add66226ca00e1d8ab52e447fbcbfb2
leemj <leemj@sph.com.sg>
2018-06-19 23:56:18 +0800

    Updated to support java8 and pdfbox version 2.0.9.

--- src/main/java/edu/emory/cellbio/ShrinkPDF.java
+++ src/main/java/edu/emory/cellbio/ShrinkPDF.java
@@ -1,47 +1,47 @@
 package edu.emory.cellbio;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import org.apache.pdfbox.cos.COSName;
+import org.apache.pdfbox.io.RandomAccessFile;
+import org.apache.pdfbox.pdfparser.PDFParser;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageTree;
+import org.apache.pdfbox.pdmodel.PDResources;
+import org.apache.pdfbox.pdmodel.graphics.PDXObject;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
+
 import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
 import javax.imageio.ImageWriteParam;
 import javax.imageio.ImageWriter;
-import javax.swing.JFileChooser;
-import javax.swing.JOptionPane;
-import org.apache.pdfbox.pdfparser.PDFParser;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
-import org.apache.pdfbox.pdmodel.PDResources;
-import org.apache.pdfbox.pdmodel.graphics.xobject.PDJpeg;
-import org.apache.pdfbox.pdmodel.graphics.xobject.PDXObject;
-import org.apache.pdfbox.pdmodel.graphics.xobject.PDXObjectForm;
-import org.apache.pdfbox.pdmodel.graphics.xobject.PDXObjectImage;
+import javax.swing.*;
+import java.awt.*;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  * App for shrinking PDF files by applying jpeg compression
- * 
+ *
  * @author Benjamin Nanes, bnanes@emory.edu
  */
 public final class ShrinkPDF {
-    
+
      // -- Fields --
-     
+
      private File input;
      private File output;
      private float compQual = -1;
      private static float compQualDefault = 0.85f;
      private boolean headless = false;
-     
+
+
      // -- Methods --
-     
+
      /**
       * Shrink using a Swing GUI to get parameters
       */
@@ -62,7 +62,7 @@ public final class ShrinkPDF {
                        JOptionPane.ERROR_MESSAGE);
           }
      }
-     
+
      /**
       * Shrink from the shell environment. Unless {@link #setHeadless(boolean) headless}
       * was set to {@code true}, falls back to GUI mode if either the
@@ -96,7 +96,7 @@ public final class ShrinkPDF {
                     JOptionPane.ERROR_MESSAGE);
          }
      }
-     
+
      /**
       * Set the input file.
       * @throws ShrinkerException if the file does not exist or
@@ -107,7 +107,7 @@ public final class ShrinkPDF {
              throw new ShrinkerException("Can't read input file: " + f==null ? "<null>" : f.toString());
          this.input = f;
      }
-     
+
      /**
       * Set the output file.
       * @throws ShrinkerException if the file does not exist and cannot
@@ -122,10 +122,10 @@ public final class ShrinkPDF {
          }
          this.output = f;
      }
-     
+
      /**
       * Set the compression quality parameter.
-      * @param compQual Number between 0 (low quality, small file size) 
+      * @param compQual Number between 0 (low quality, small file size)
       *                 and 1 (high quality, large file size)
       * @throws ShrinkerException if {@code compQual} is out of bounds
       */
@@ -134,16 +134,16 @@ public final class ShrinkPDF {
              throw new ShrinkerException("Compression quality must be between 0 and 1");
          this.compQual = compQual;
      }
-     
+
      /**
       * Set {@code true} to prohibit the use of dialogs.
       */
      public void setHeadless(final boolean headless) {
          this.headless = headless;
      }
-     
+
      // -- Helper methods --
-     
+
      /**
       * Use a Swing-based dialog to select the input file.
       * @throws ShrinkerException if the dialog is canceled
@@ -157,16 +157,16 @@ public final class ShrinkPDF {
             if(jfc.showOpenDialog(null) != JFileChooser.APPROVE_OPTION)
                   throw new ShrinkerException("Canceled by user.");
             input = jfc.getSelectedFile();
-            try { 
+            try {
                 setInput(input);
             } catch(ShrinkerException e) {
-                JOptionPane.showMessageDialog(null, e.getMessage(), 
-                        "Shrunken heads, there's been an error!", 
+                JOptionPane.showMessageDialog(null, e.getMessage(),
+                        "Shrunken heads, there's been an error!",
                         JOptionPane.ERROR_MESSAGE);
             }
         }
      }
-     
+
      /**
       * Use a Swing-based dialog to select the compression
       * quality parameter.
@@ -174,17 +174,18 @@ public final class ShrinkPDF {
       */
      private void selectCompQualUI() throws ShrinkerException {
          compQual = -1;
-         while(compQual < 0 || compQual >1) {
-             final String compString = 
+         while(compQual < 0.0 || compQual > 1.0) {
+             final String compString =
                   JOptionPane.showInputDialog(
                   "Pick a number between 0 (lowest quality) "
-                  + "and 100 (largest size):", "85");
+                  + "and 10 (largest size):", "8");
+             System.out.println(compString);
              if(compString == null || compString.isEmpty())
                  throw new ShrinkerException("Canceled by user.");
              try {
-                 compQual = Math.max(Math.min(Float.valueOf(compString), 100), 0)/100;
+                 compQual = Math.max(Math.min(Float.valueOf(compString), 10), 0)/10;
              } catch(NumberFormatException e) {
-                 JOptionPane.showMessageDialog(null, compString 
+                 JOptionPane.showMessageDialog(null, compString
                          + " isn't any sort of number I recognize!",
                          "Shrunken heads, there's been an error!",
                          JOptionPane.ERROR_MESSAGE);
@@ -192,7 +193,7 @@ public final class ShrinkPDF {
              }
          }
      }
-     
+
      /**
       * Use a Swing-based dialog to select an output file.
       * @throws ShrinkerException if the dialog is canceled
@@ -200,61 +201,47 @@ public final class ShrinkPDF {
      private void selectOutputUI() throws ShrinkerException {
          output = null;
          while(output == null) {
-            JFileChooser jfc = new JFileChooser();
-            jfc.setDialogType(JFileChooser.SAVE_DIALOG);
-            jfc.setDialogTitle("Select destination for shrunken PDF...");
-            if(jfc.showSaveDialog(null) != JFileChooser.APPROVE_OPTION)
-                  throw new ShrinkerException("Canceled by user.");
-            output = jfc.getSelectedFile();
-            try { 
+             JFileChooser jfc = new JFileChooser();
+             jfc.setDialogType(JFileChooser.SAVE_DIALOG);
+             jfc.setDialogTitle("Select destination for shrunken PDF...");
+             if(jfc.showSaveDialog(null) != JFileChooser.APPROVE_OPTION)
+                 throw new ShrinkerException("Canceled by user.");
+             output = jfc.getSelectedFile();
+            try {
                 setOutput(output);
             } catch(ShrinkerException e) {
-                JOptionPane.showMessageDialog(null, e.getMessage(), 
-                        "Shrunken heads, there's been an error!", 
+                JOptionPane.showMessageDialog(null, e.getMessage(),
+                        "Shrunken heads, there's been an error!",
                         JOptionPane.ERROR_MESSAGE);
             }
          }
      }
-     
-     /**
-      * Shrink a PDF
-      * @param f {@code File} pointing to the PDF to shrink
-      * @param compQual Compression quality parameter. 0 is
-      *                 smallest file, 1 is highest quality.
-      * @return The compressed {@code PDDocument}
-      * @throws FileNotFoundException
-      * @throws IOException 
-      */
-     private PDDocument shrinkMe() 
+
+     private PDDocument shrinkMe()
              throws FileNotFoundException, IOException {
-          if(compQual < 0)
+            if(compQual < 0)
               compQual = compQualDefault;
-          final FileInputStream fis = new FileInputStream(input);
-          final PDFParser parser = new PDFParser(fis);
-          parser.parse();
-          final PDDocument doc = parser.getPDDocument();
-          List pages = doc.getDocumentCatalog().getAllPages();
-          for(Object p : pages) {
-               if(!(p instanceof PDPage))
-                    continue;
-               PDPage page = (PDPage) p;
-               scanResources(page.getResources(), doc);
-          }
-          return doc;
+            final PDFParser parser = new PDFParser(new RandomAccessFile(input,"r"));
+            parser.parse();
+            final PDDocument doc = parser.getPDDocument();
+
+            PDPageTree pageTree = doc.getDocumentCatalog().getPages();
+            for (PDPage pdPage : pageTree) {
+                scanResources(pdPage.getResources(), doc);
+            }
+            return doc;
      }
-     
+
      private void scanResources(final PDResources rList, final PDDocument doc)
            throws FileNotFoundException, IOException {
-         Map<String, PDXObject> xObs = rList.getXObjects();
-         for(String k : xObs.keySet()) {
-            final PDXObject xObj = xObs.get(k);
-            if(xObj instanceof PDXObjectForm)
-                 scanResources(((PDXObjectForm) xObj).getResources(), doc);
-            if(!(xObj instanceof PDXObjectImage))
-                 continue;
-            PDXObjectImage img = (PDXObjectImage) xObj;
-            System.out.println("Compressing image: " + k);
-            final Iterator<ImageWriter> jpgWriters = 
+
+         Map<COSName,PDImageXObject> imageXObjectMap = new HashMap<>();
+
+         Iterable<COSName> cosNames= rList.getXObjectNames();
+         for(COSName cosName : cosNames) {
+            final PDXObject xObj = rList.getXObject(cosName);
+            System.out.println("Compressing image: " + cosName);
+            final Iterator<ImageWriter> jpgWriters =
                     ImageIO.getImageWritersByFormatName("jpeg");
             final ImageWriter jpgWriter = jpgWriters.next();
             final ImageWriteParam iwp = jpgWriter.getDefaultWriteParam();
@@ -263,15 +250,18 @@ public final class ShrinkPDF {
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
             jpgWriter.setOutput(ImageIO.createImageOutputStream(baos));
             jpgWriter.write(null,
-                    new IIOImage(img.getRGBImage(), null, null), iwp);
-            ByteArrayInputStream bais = 
-                    new ByteArrayInputStream(baos.toByteArray());
-            PDJpeg jpg = new PDJpeg(doc, bais);
-            xObs.put(k, jpg);
+                    new IIOImage(((PDImageXObject) xObj).getImage(), null, null), iwp);
+
+             PDImageXObject pdImageXObject = PDImageXObject
+                     .createFromByteArray(doc,baos.toByteArray(),cosName.getName());
+             imageXObjectMap.put(cosName,pdImageXObject);
          }
-         rList.setXObjects(xObs);
+
+         imageXObjectMap.entrySet().forEach(object -> {
+             rList.put(object.getKey(),object.getValue());
+         });
      }
-     
+
      // -- Main methods --
 
      /**
